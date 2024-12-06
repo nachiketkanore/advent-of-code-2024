3de8be653e0b990600b628842251fdb855c1bf8f
Nachiket Kanore <nachiket.kanore@gmail.com>
2024-12-06 19:47:23 +0530

    day 06 - solved part 02 finally

--- python/day06/sol2.py
+++ python/day06/sol2.py
@@ -1,13 +1,11 @@
 import sys
+from tqdm import tqdm
 sys.setrecursionlimit(10**7)
 
 mat = open('ex').read().strip().split('\n')
 mat = [[ch for ch in row] for row in mat]
 N, M = len(mat), len(mat[0])
-# print(N, M)
-# print(mat)
-
-vis = [[0 for _ in range(N)] for _ in range(M)]
+print(N, M)
 
 def inside(i, j):
     return i >= 0 and i < N and j >= 0 and j < M
@@ -19,14 +17,10 @@ def go(i, j, dir):
         return False
     if mat[i][j] == '#':
         return False
-    if vis[i][j] >= 10:
+    if (i, j, dir) in check:
         return True
-        # if (i, j, dir) in check:
-        #     return True
 
-    vis[i][j] += 1
-    # if vis[i][j] > 1:
-    #     check.add((i, j, dir))
+    check.add((i, j, dir))
 
     dx, dy = 0, 0
     if dir == 'u':
@@ -37,21 +31,19 @@ def go(i, j, dir):
         dx, dy = 0, -1
     elif dir == 'r':
         dx, dy = 0, +1
-    else:
-        assert(False)
 
     ni, nj = i + dx, j + dy
 
     if inside(ni, nj):
         if mat[ni][nj] == '#':
             if dir == 'u':
-                return go(i, j + 1, 'r')
+                return go(i, j, 'r')
             elif dir == 'd':
-                return go(i, j - 1, 'l')
+                return go(i, j, 'l')
             elif dir == 'l':
-                return go(i - 1, j, 'u')
+                return go(i, j, 'u')
             elif dir == 'r':
-                return go(i + 1, j, 'd')
+                return go(i, j, 'd')
         else:
             return go(ni, nj, dir)
 
@@ -72,26 +64,16 @@ for i in range(N):
 
 progress = 0
 ans = 0
-for i in range(N):
+for i in tqdm(range(N)):
     for j in range(M):
         progress += 1
         if mat[i][j] == '.':
-
-            for x in range(N): 
-                for y in range(M): 
-                    vis[x][y] = 0
 
             mat[i][j] = '#'
             check.clear()
 
             if go(si, sj, 'u'):
                 ans += 1
-                mat[i][j] = 'X'
-                # for x in range(N): 
-                #     print([''.join(v) for v in mat[x]])
-                # print()
-                # for x in range(N): 
-                #     print([v for v in vis[x]])
 
             mat[i][j] = '.'
 
