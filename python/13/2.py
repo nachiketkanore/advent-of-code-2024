22c6d8a717c27cc492ef3aec9a671f71fa414641
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2024-12-13 12:30:32 +0530

    solved day 13 part two in python

---
+++ python/13/2.py
@@ -0,0 +1,22 @@
+import re
+lines = open('in').read().strip().split('\n\n')
+
+INF = int(1e20)
+ADD = 10000000000000
+ans = 0
+for line in lines:
+    nums = re.findall(r'\d+', line)
+    ax, ay, bx, by, X, Y = map(int, nums)
+    X += ADD
+    Y += ADD
+
+    B = (X * ay - Y * ax) // (bx * ay - ax * by)
+    best = INF
+    A1 = (X - B * bx) // ax
+    A2 = (Y - B * by) // ay
+    for A in (A1, A2):
+        if (A * ax + B * bx) == X and (A * ay + B * by) == Y:
+            best = min(best, 3 * A + B)
+    if best < INF:
+        ans += best
+print(ans)
