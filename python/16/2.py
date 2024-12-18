d74c98a672f82caf1e61f4466cd212df16630152
Nachiket Kanore <nachiket.kanore@gmail.com>
2024-12-19 00:42:30 +0530

    tried to optimize but not enough

--- python/16/2.py
+++ python/16/2.py
@@ -24,21 +24,29 @@ INF = int(1e9)
 
 dirs = [ (0, 1), (-1, 0), (0, -1), (1, 0) ]
 
-best = 82460
+# 11048(ans = 64) ex2 
+# 7036(ans = 45) ex1
+best = 82460 
+TURNS = best // 1000
+STRAIGHTS = best % 1000
 cells = set()
 visited = set()
-def dfs(x, y, dir, cost, trace=[]):
-    global cnt, best, cells
-    if mat[x][y] == '#' or cost > best or (x, y) in visited:
+
+def dfs(x, y, dir, turns, straight):
+    global cells
+    if mat[x][y] == '#' or turns > TURNS or straight > STRAIGHTS or (x, y) in visited:
         return
     if mat[x][y] == 'E':
-        print(f'checking ans = {len(cells)}')
-        if cost == best:
-            for a, b in trace:
+        print('here', turns, straight)
+        if turns == TURNS and straight == STRAIGHTS:
+            cells.add((x, y))
+            print(f'found ans = {len(cells)}')
+            for a, b in visited:
                 cells.add((a, b))
         return
 
     visited.add((x, y))
+    # print(visited)
     # print(trace)
 
     idlx, idrx = (dir - 1 + 4) % 4, (dir + 1) % 4
@@ -48,18 +56,18 @@ def dfs(x, y, dir, cost, trace=[]):
         # print(f'here for {x}, {y}, checking for {nx}, {ny}')
         if inside(nx, ny) and mat[nx][ny] != '#' and (nx, ny) not in visited:
             # print(f'going from ({x}, {y}) to ({nx}, {ny})')
-            dfs(nx, ny, j, 1000 + cost + 1, trace + [(nx, ny)])
+            dfs(nx, ny, j, turns + 1, straight + 1)
 
     dx, dy = dirs[dir]
     nx, ny = x + dx, y + dy
     # print(f'here for {x}, {y}, diffs = {dx, dy} checking for {nx}, {ny}')
     if inside(nx, ny) and mat[nx][ny] != '#' and (nx, ny) not in visited:
         # print(f'going from ({x}, {y}) to ({nx}, {ny})')
-        dfs(nx, ny, dir, cost + 1, trace + [(nx, ny)])
+        dfs(nx, ny, dir, turns, straight + 1)
 
     visited.remove((x, y))
 
-dfs(si, sj, 0, 0, [(si, sj)])
+dfs(si, sj, 0, 0, 0)
 
 print(f'best = {best}')
 print(f'ans = {len(cells)}')
